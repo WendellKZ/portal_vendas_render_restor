44473aeb8eeec5e359b6dd1339971ead53079989
WendellKZ <wendell.costa@liderbrinquedos.com.br>
2025-09-03 14:08:10 -0300

    chore(drf): habilita /api-auth/login/ na UI do DRF

--- portal_vendas/urls.py
+++ portal_vendas/urls.py
@@ -1,16 +1,10 @@
-# portal_vendas/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-
-    # API REST
     path("api/", include("api.urls")),
-
-    # Login/Logout do DRF (para usar o botão "Login" na UI do /api/)
-    path("api-auth/", include("rest_framework.urls")),
-
-    # App do portal
+    path("api-auth/", include("rest_framework.urls")),  # UI do DRF (login/logout)
     path("app/", include(("core.urls_front", "portal"), namespace="portal")),
 ]
+
